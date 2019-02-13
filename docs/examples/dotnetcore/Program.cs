416aa1de5fb6986b86b9267afd078e9a71011ba1
Kevin Hampton <khampton6@gmail.com>
2019-02-12 16:51:24 -0800

    Adding a .net core sample project on how I read and parse the certificate from the KeyVault volume.

---
+++ docs/examples/dotnetcore/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace dotnetcore
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Input parsing.
+            Console.WriteLine("Please enter the path to the KeyVault files or volume.");
+            string folderPath = Console.ReadLine();
+
+            Console.WriteLine("Please enter the secret name.");
+            string secretName = Console.ReadLine();
+
+            string fullPath = Path.Combine(folderPath, secretName);
+
+            // Verifying secret file exists and path is OK.
+            Console.WriteLine($"Reading secret at location: {fullPath}");
+            if (!File.Exists(fullPath))
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }          
+
+            try
+            {
+                // Read all of the secret contents and convert to Byte array.
+                string secretContents = File.ReadAllText(fullPath);
+                Byte[] rawCertContent = Convert.FromBase64String(secretContents);
+
+                // The constructor parses the Byte array into the certificate in memory.
+                // If you need to enter a password, then there's an overloaded constructor with
+                // the string password as the second parameter.
+                X509Certificate2 certificate = new X509Certificate2(rawCertContent);
+
+                Console.WriteLine("Successfully parsed the secret into a certificate");
+                Console.WriteLine($"Subject name: {certificate.SubjectName.Name}");
+                Console.WriteLine($"Thumbprint: {certificate.Thumbprint}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception thrown while trying to read or parse the secret into a certificate.");
+                Console.WriteLine($"Message: {e.Message}");
+            }
+        }
+    }
+}
---
+++ docs/examples/dotnetcore/README.md
@@ -0,0 +1,21 @@
+These examples were ran using .net core version 2.2 as a way to read the certificate secrets and convert them into X509Certificate2 objects. 
+
+How to run the examples:
+1. Download and install .netcore 2.2 SDK
+2. Clone the enlistment or copy the code.
+3. Build by running the command 'dotnet build'
+4. Run by running the command 'dotnet run'
+5. You'll be prompted for the volume/directory path along with the secret name.
+
+How to create a local test secret file.
+1. Ensure that you created the certificate in Azure Key Vault.
+2. Ensure that you have installed the Azure CLI tools.
+3. Open bash or powershell and login with 'az login' command. You might also need to set the default subscription.
+4. Get the secret via one of the following commands:
+    Bash:  'az keyvault secret show --vault-name <vaultname> --name <certificatename>' and the secret will show under 'value' in the json blob response.
+    Powershell: '$secret = Get-AzureKeyVaultSecret -VaultName <vaultname> -Name <certificatename>' then you can enter '$secret.SecretValueText' to see the contents you need.
+5. Copy the value to an empty file with no file extension (similarly to how the KV Flex Vol pod).
+
+Notes:
+1. The certificate was added to Azure KeyVault as a cert, but is referenced in our deployment yaml as a secret.
+2. The certificate used in this example was self signed and with a password. I am able to read the cert secret without the password. However, you might need the password if you want to install the cert on the machine.
