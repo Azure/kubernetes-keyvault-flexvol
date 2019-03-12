0db0ddabd1da233e883bdb08f10376ed998920e8
Rita Zhang <rita.z.zhang@gmail.com>
2019-03-12 12:53:15 -0700

    bump version (#89)

--- azurekeyvault-flexvolume/main.go
+++ azurekeyvault-flexvolume/main.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	program                = "azurekeyvault-flexvolume"
-	version                = "0.0.9"
+	version                = "0.0.10"
 	permission os.FileMode = 0644
 	objectsSep             = ";"
 )
