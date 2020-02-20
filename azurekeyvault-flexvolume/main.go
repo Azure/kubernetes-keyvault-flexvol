257ea1dc405b8193c998b627315ffab9004769f1
Anish Ramasekar <anish.ramasekar@gmail.com>
2020-02-20 13:27:22 -0800

    bump version to v0.0.16

--- azurekeyvault-flexvolume/main.go
+++ azurekeyvault-flexvolume/main.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	program                = "azurekeyvault-flexvolume"
-	version                = "0.0.15"
+	version                = "0.0.16"
 	permission os.FileMode = 0644
 	objectsSep             = ";"
 )
